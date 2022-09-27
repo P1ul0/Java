f83099bb00591ecbbd5da4442b32586e3f4260cc
Paulo <102394018+P1ul0@users.noreply.github.com>
2022-09-27 18:42:13 -0300

    update Lista.java

--- src/entidade/Lista.java
+++ src/entidade/Lista.java
@@ -3,27 +3,10 @@ public class Lista {
     private No inicio;
     private No ultimo;
 
-    public Lista() {
-        this.inicio = null;
-        this.ultimo = null;
-    }
-
-    public No getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(No inicio) {
-        this.inicio = inicio;
-    }
-
-    public No getUltimo() {
-        return ultimo;
-    }
-
-    public void setUltimo(No ultimo) {
-        this.ultimo = ultimo;
-    }
-    
+ public Lista() {
+      this.inicio = null;
+      this.ultimo = null;
+   }
   public void adicionar(int novoValor){
       No novoNo = new No(novoValor);
       No p = this.inicio;
